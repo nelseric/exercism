41e4b08f2f8c987e90f8efc88041dfd1e5c90654
Eric Nelson <nelseric.alt@gmail.com>
2016-12-19 12:56:06 -0600

    Add start of food chain

---
+++ ecmascript/food-chain/food-chain.js
@@ -0,0 +1,11 @@
+class Song {
+  verse(verseNum) {
+    if (verseNum === 1) {
+      return `I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+`;
+    }
+  }
+}
+
+export default Song;
